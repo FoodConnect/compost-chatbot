9bedd21f23dfd0e59e982b54aa767dea86e2d145
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-05-17 18:21:46 -0700

    adds new lambda handler method and adds new github workflow, gitignores all pycache directories

---
+++ vector_embeddings_handler/lambda_function.py
@@ -0,0 +1,22 @@
+import os
+import json
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain_core.prompts import PromptTemplate
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+
+def lambda_handler(event, context):
+  try:
+    return {
+      'statusCode': 200,
+      'body': 'Interaction successful!'
+    }
+  except Exception as e:
+    return {
+      'statusCode': 500,
+      'body': str(e)
+    }
