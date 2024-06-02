d983f00fd37ffecb017238574b482bc152536ee7
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-06-01 19:02:09 -0700

    Defines data ingestion handler function with Dynamodb item attributes

--- data_ingestion_handler/lambda_function.py
+++ data_ingestion_handler/lambda_function.py
@@ -1,20 +1,36 @@
+import json
 import boto3
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain_core.prompts import PromptTemplate
-from langchain_text_splitters import CharacterTextSplitter
-from langchain_community.vectorstores import Chroma
-import faiss
+import fitz
 
 
 def lambda_handler(event, context):
   try:
+    s3_client = boto3.client('s3')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('DocumentMetadata')
+
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    response = s3_client.get_object(Bucket=bucket_name, Key=key)
+    file_content = response['Body'].read()
+
+    document = fitz.open("pdf", file_content)
+    text = ""
+    for page in document:
+          text += page.get_text()
+
+    response = table.put_item(
+          Item={
+              'documentId': key,
+              'text': text,
+              'title': 'Extracted from PDF',  
+          }
+      )
     return {
-      'statusCode': 200,
-      'body': 'Interaction successful!'
-    }
+          'statusCode': 200,
+          'body': json.dumps('Text successfully extracted and stored.')
+      }
   except Exception as e:
     return {
       'statusCode': 500,
